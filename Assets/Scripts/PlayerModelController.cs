4a75df486a8b77d7695fb3145aadb196427cf610
WilliamAB <liams_1998@hotmail.com>
2021-11-28 03:08:30 +0200

    Add player proto model + animation

---
+++ Assets/Scripts/PlayerModelController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerModelController : MonoBehaviour
+{
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
